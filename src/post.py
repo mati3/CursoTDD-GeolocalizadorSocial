a99593119714c37f981d86480fcbc3d73b5b94a6
mati <mati331@correo.ugr.es>
2019-10-30 14:05:56 +0100

    add clase post.py

---
+++ src/post.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+class Post:
+    
+    def Post (autor, tags, social, location):
+        self.autor = autor
+        self.tags = tags
+        self.social = social
+        self.location = location
+        
+    def getAutor():
+        return self.autor
+    
+    def getTags():
+        return self.tags
+    
+    def getSocial():
+        return self.social
+    
+    def getLocation():
+        return self.location
+    
+
