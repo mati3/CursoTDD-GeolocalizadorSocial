cd5ab84df4395e376395f10273ec5301a68e4824
toniMR <toni97sk8@gmail.com>
2019-10-31 09:39:20 +0100

    Añadiendo test creación Post

---
+++ tests/test_Post.py
@@ -0,0 +1,22 @@
+import sys, os.path
+
+src = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
++ '/src/')
+sys.path.append(src)
+
+import unittest
+
+from post import Post
+
+class TestCine(unittest.TestCase):
+
+    # Comprueba que en la ruta devuelve status OK
+    def test_create_post(self):
+        tags = ["test", "python"]
+        p = Post("Federico", tags, "Twitter", "Granada")
+
+        self.assertEqual(p.getAutor(),"Federico", "Autor devuelto incorrecto")
+        self.assertEqual(p.getTags(), tags, "Tags devueltos incorrectos")
+        self.assertEqual(p.getSocial(),"Twitter", "Red social devuelta incorrecta")
+        self.assertEqual(p.getLocation(),"Granada", "Localizacion devuelta incorrecta")
+        
